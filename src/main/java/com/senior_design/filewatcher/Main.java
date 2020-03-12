0ebee452460ab530e5179c492bcd184c13fb7a6c
Alys Breckel <alys.breckel@gmail.com>
2020-03-11 21:27:51 -0500

    Recognize LinkedIn watermark

--- src/main/java/com/senior_design/filewatcher/Main.java
+++ src/main/java/com/senior_design/filewatcher/Main.java
@@ -1,11 +1,21 @@
 package com.senior_design.filewatcher;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Test");
         Arguments parsedArgs = new Arguments(args);
-        FileWatcher fw = new FileWatcher(parsedArgs);
-        fw.run().join();
+        try (PDFParser p = new PDFParser(new File(parsedArgs.getWatchPath()))) {
+            p.splitPages();
+//            String[] text = p.text();
+//            for (int i = 0; i < text.length; i += 1) {
+//                System.out.println("Page: " + i);
+//                System.out.println(text[i]);
+//            }
+        }
+//        FileWatcher fw = new FileWatcher(parsedArgs);
+//        fw.run().join();
     }
 
 }
