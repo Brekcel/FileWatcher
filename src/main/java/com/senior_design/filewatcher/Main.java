564efc4c5aaeb80ba41357794db722cb4ebedb0a
Alys Breckel <alys.breckel@gmail.com>
2020-04-02 21:17:11 -0500

    Connect FileWatcher and PDFParser

--- src/main/java/com/senior_design/filewatcher/Main.java
+++ src/main/java/com/senior_design/filewatcher/Main.java
@@ -1,21 +1,10 @@
 package com.senior_design.filewatcher;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
-
-import java.io.File;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Test");
         Arguments parsedArgs = new Arguments(args);
-        try (PDFParser p = new PDFParser(parsedArgs, new File("./watch/linkedin.big.pdf"))) {
-            PDDocument[] docs = p.splitDoc();
-            for (int i = 0; i < docs.length; i += 1) {
-                docs[i].save("./saved/resume " + i + ".pdf");
-            }
-        }
-//        FileWatcher fw = new FileWatcher(parsedArgs);
-//        fw.run().join();
+        FileWatcher fw = new FileWatcher(parsedArgs);
+        fw.run();
     }
 
 }
